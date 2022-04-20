fd6613d84cc2d4ac6d23b68ef74253efb3e5f289
sporovich <phgpablito@gmail.com>
2022-04-20 23:45:10 +0200

    pruebas nav
    pruebas en el nav

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,7 @@
         <title>{{ config('app.name', 'Proyecto Final Daw') }}</title>
         <!-- Styles -->
         <!--<link href="{asset ('css/app.css')}" rel="stylesheet">-->
-        <link href="{{ mix('css/app.css') }}" rel="stylesheet" type="text/css" />
+        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
         <!-- Bootstrap JS -->
@@ -24,8 +24,7 @@
     <div class="container-fluid">
         <div class="logo">
             <a class="navbar-brand" href="#">
-                <img src="../public/img/mordecai.jpeg" alt="logo" width="30" height="30" class="d-inline-block align-text-top">
-                Pablo_hg
+                {{ Html::image('img/mordecai.jpeg', 'Logo Pablohg',['class'=>'imagenlogo']) }}                Pablo_hg
             </a>
         </div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
@@ -34,10 +33,10 @@
         <div class="collapse navbar-collapse flex-row-reverse" id="navbarNavDropdown">
             <ul class="navbar-nav">
                 <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    <a class="nav-link active" aria-current="page" href="{{ route('home') }}">Home</a>
                 </li>
                 <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="projects" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <a class="nav-link dropdown-toggle" href="{{ route('projects') }}" id="navbarDropdown">
                         Projects
                     </a>
                     <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
@@ -57,9 +56,6 @@
         </div>
     </div>
 </nav>
-    <!--Menú de navegación movil-->
-
-
     <main>
         <header>
             <h1>Mi primer CMS</h1>
